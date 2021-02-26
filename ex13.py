efb2919dd52ad940d7a11eecbb60db21135b8a9d
Jayson Duff <jayson.duff@gsgus.com>
2021-02-26 16:59:33 -0500

    variable prompt exp, possible outdated syntax

--- ex13.py
+++ ex13.py
@@ -3,7 +3,7 @@ from sys import argv
 script, first, second, third = argv
 
 
-print(">>>> argv= " repr(argv))
+print(">>>> argv= ", repr(argv))
 
 print("The script is called:", script)
 print("Your first variable is:", first)
