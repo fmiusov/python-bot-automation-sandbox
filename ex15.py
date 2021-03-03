67c464c6a28696851db87f91b1256b97c4bb4694
Jayson Duff <jayson.duff@gsgus.com>
2021-03-03 13:53:25 -0500

    basic text editor

--- ex15.py
+++ ex15.py
@@ -1,15 +1,22 @@
+# import argument module for script
 from sys import argv
 
+# designate how to parse arguments
 script, filename = argv
 
+# assign the txt variable as the function to open the filename argument received by argv
 txt = open(filename)
 
+# print first line telling user their filename title, then print the results of .read() called off the txt variable
 print(f"Here's your file {filename}:")
 print(txt.read())
 
+# prompt the user to type their filename again, log the input under variable file_again
 print("Type the filename again:")
 file_again = input("> ")
 
+# assign txt_again variable the file contents of file_again variable, ie the filename 
 txt_again = open(file_again)
 
+# print the the result of .read() of txt_again variable
 print(txt_again.read())
